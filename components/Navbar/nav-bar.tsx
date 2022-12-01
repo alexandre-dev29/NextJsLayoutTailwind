100f4d638631a0b41650f2b01ad24cbe6b1321bb
alexandre-dev29 <axel.business29@gmail.com>
2022-12-01 10:43:26 +0200

    First finish of the nav bar

--- components/Navbar/nav-bar.tsx
+++ components/Navbar/nav-bar.tsx
@@ -2,10 +2,19 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className={"bg-teal-600 min-h-[40px] h-[5%] flex justify-between items-center "}>
-      <div>sdasd</div>
-      <div>asdasd</div>
-    </nav>
+    <div className={"shadow-md min-h-[40px] h-[5%] flex justify-between items-center px-8"}>
+      <div className={"flex items-center"}>
+        <div className={"pr-4 pb-2 pt-1 border-r-2"}>
+          <p className={"bg-teal-400 px-2 py-1 rounded-md"}>ST</p>
+        </div>
+        <h2 className={"ml-4"}>Alexandre mwenze</h2>
+      </div>
+      <div className={"pl-4 pb-2 pt-1 border-l-2"}>
+        <a href={"#"} className={"bg-violet-500 rounded-full py-2 px-1"}>
+          Dan
+        </a>
+      </div>
+    </div>
   );
 };
 
